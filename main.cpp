e73fe4216c043889b67866dc970137ba3122e5e2
Christine Susic <csusic022@insite.4cd.edu>
2026-04-04 16:07:53 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -2,35 +2,61 @@
 
 #include <iostream>
 #include <chrono>
+#include <list>
+#include <set>
 using namespace std;
+
+const int SZ = 20000;  //size of array from file
 
 int main() {
     //output
     cout << "Operation   Vector   List   Set";
     
-    //read data
+    //READ DATA
     cout << "Read";
-    //read data into vector
-    //read data into list
-    //read data into set
+    // first read names file and store in an array
+    ifstream fin("codes.txt");
+    string codes[SZ];
+    int i = 0;
+    while (fin >> codes[i++]);
+    fin.close();
     
-    //sort data
+     //read data into vector
+
+    //read data into list
+    //list of codes
+    list<string> codes;
+    
+    //read data into set
+    //set of codes
+    set<string> codes;
+    
+    
+    //SORT DATA
     cout << "Sort";
     //sort data into vector
+    
     //sort data into list
+    
     //sort data into set
     
-    //insert data
+    
+    //INSERT DATA
     cout << "Insert";
     //insert data into vector
-    //insert data into
-    //insert data into
     
-    //delete data
+    //insert data into list
+    
+    //insert data into set
+    
+    
+    //DELETE DATA
     cout << "Delete";
     //delete data from vector
-    //delete data from
-    //delete data from
+    
+    //delete data from list
+    
+    //delete data from set
     
 
     return 0;

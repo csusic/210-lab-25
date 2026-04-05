3c27877125bcae430c32e30ac86539207cfbab46
Christine Susic <csusic022@insite.4cd.edu>
2026-04-04 18:16:36 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // COMSC-210 | Lab 25 | Christine Susic
 
 #include <iostream>
+#include <iomanip>
 #include <chrono>
 #include <algorithm>
 #include <fstream>
@@ -10,11 +11,15 @@
 using namespace std;
 using namespace std:: chrono;
 
-const int SZ = 20000;  //size of array from file
+//size of array from file and column widths
+const int SZ = 20000, W1 = 12, W2 = 9, W3 = 7, W4 = 6;  
 
 int main() {
     //output
-    cout << "Operation   Vector   List   Set" << end
+    cout << setw(W1) << left << "Operation";
+    cout << setw(W2) << "Vector";
+    cout << setw(W3) << "List";
+    cout << setw(W4) << "Set" << endl;
     
     //vector of codes
     vector<string> vectorCodes;
@@ -24,7 +29,7 @@ int main() {
     set<string> setCodes;
     
     //READ DATA
-    cout << "Read" << endl;
+    cout << setw(W1) << "Read" << endl;
     //read names file and store it in a vector, list, and set
     ifstream fin("codes.txt");
     string codes;
@@ -41,16 +46,29 @@ int main() {
     fin.close();
  
     //SORT DATA
-    cout << "Sort" << endl;
+    cout << setw(W1) << "Sort";
+    
+    //Vector Sort
+    //start timing
+    auto start = high_resolution_clock::now();
     //sort data into vector
     sort(vectorCodes.begin(), vectorCodes.end());
+    //end timing
+    auto end = high_resolution_clock::now();
+     //calculate duration
+    auto duration = duration_cast<nanoseconds>(end - start);
+    //output the duration in nanoseconds
+    cout << duration.count();
+    
+    //List Sort
     //sort data into list
     listCodes.sort();
     //sort data into set
     int setSorted = -1;
+    cout << endl;
     
     //INSERT DATA
-    cout << "Insert" << endl;
+    cout << setw(W1) << "Insert";
     //insert data into vector
     vectorCodes.insert(vectorCodes.begin() + vectorCodes.size() / 2, "TESTCODE");
     //insert data into list
@@ -59,9 +77,10 @@ int main() {
     listCodes.insert(it, "TESTCODE");
     //insert data into set
     setCodes.insert("TESTCODE");
+    cout << endl;
     
     //DELETE DATA
-    cout << "Delete" << endl;
+    cout << setw(W1) << "Delete";
     //delete data from vector
     vectorCodes.erase(vectorCodes.begin() + vectorCodes.size() / 2);
     //delete data from list
@@ -71,6 +90,7 @@ int main() {
     auto it1 = setCodes.begin();
     advance(it1, setCodes.size() / 2);
     setCodes.erase(it1);
+    cout << endl;
 
     return 0;
 }
